23ed9b57c4b71fbbba3d92b153de283d5b761d19
Umer Aadil <39928587+Umeraadil@users.noreply.github.com>
2020-12-21 12:56:55 +0530

    Add files via upload

---
+++ swapping with and without 3rd variable .py
@@ -0,0 +1,16 @@
+print("CODE BY UMER AADIL")
+print("SWAPPING OF TWO NUMBERS WITHOUT THIRD VARIABLE")
+a = 2
+b = 3
+print("numbers before swaping")
+print(a,b)
+print("numbers after swapping using 3rd variable")
+# swaping_with_3rd_variable
+c = a
+a = b
+b = c
+print(a,b)
+# awaping without 3rd variable
+print("NUMBERS AFTER SWAPING WITHOUT 3RD VARIABLE")
+a,b=b,a
+print(a,b)
